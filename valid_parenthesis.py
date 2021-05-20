f0478dc5965688211dd31f02464e36529ad64f76
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-05-20 15:43:45 -0400

    - initial sol

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        if len(s) == 1:
+            return False
+        dic = {'(': 1, '[': 2, '{': 3, ')': 4, ']': 5, '}': 6}
+        stack = []
+        valid = True
+        for i in range(0, len(s)):
+            curr = dic[s[i]]
+            if curr < 4:
+                stack.append(curr)
+            else:
+                if stack.pop() != curr-3:
+                    valid = False
+                    break
+        return valid
+
+
+a = Solution()
+print(a.isValid("()"))
